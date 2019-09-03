24ea9a182d33d2100e89496b5b241db88bbbc63e
Joseph Doucet <sssd09@gmail.com>
2019-09-03 14:28:37 -0400

    Removed more provided comments and console logs

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 //Here I create the array of quotes
+//Using simple letter strings as filler while I flush out the code
 let quotes = [
   {
     quote: 'A',
@@ -50,43 +51,34 @@ let quotes = [
     year: 1981
   }
 ]
-//Testing that the array is correct
-console.log(quotes);
+
 //Here I create the function to generate a random quote object from the array
 function getRandomQuote()  {
-  //here I generate a random number from 0 to 9
+  //Here I generate a random number from 0 to 9
   let randomNumber = (Math.floor(Math.random() * quotes.length));
   //And now I assign the newly created number to a quote object and the return the quote
   return quotes[randomNumber];
 }
-//Testing that the random quote is generated
-console.log(getRandomQuote());
 
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
+//Here I create the printQuote function
 function printQuote() {
+  //I set the randomly generated quote to a variable for to access it easily
   let randomQuote = getRandomQuote();
+  //I create an empty message string to concatenate onto
   let message = "";
+  //I concatenate the message string with the quote and source
   message += '<p class="quote">' + randomQuote.quote + '</p>';
   message += '<p class= "source">' + randomQuote.source;
-  //Need to figure out a way to get the conditionals to work
+  //Here I check to see if the quote is missing the citation key
+  //If it is not, I add the citation to the quote
     if (randomQuote.citation !== undefined) {
       message += '<span class="citation">' + randomQuote.citation + '</span>'
     }
+    //Same process for the year key as for the citation key
     if(randomQuote.year !== undefined) {
      message += '<span class="year">' + randomQuote.year + '</span>'
     }
+    //After checking for the citation and year, I end the message string
   message += '</p>';
   //Using getElementById from MDN to try to target the 'quote box' div
 document.getElementById("quote-box").innerHTML = message;
@@ -95,6 +87,3 @@ document.getElementById("quote-box").innerHTML = message;
 
 //Reminder: DO NOT TOUCH THIS CODE.
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
